7dd9aaae936051a58f3ea7f7ee83aa0953a44a5d
John Dean <johnldean@github.com>
2018-06-05 21:14:50 -0700

    cleaned up TF

--- car-rocket-ship_TL_testing.py
+++ car-rocket-ship_TL_testing.py
@@ -9,32 +9,59 @@ from keras.layers import Dense, Activation
 from keras import regularizers
 import matplotlib.pyplot as plt
 import numpy as np
-img_width, img_height = 256, 256
-train_data_dir = "car_rocket/train"
-validation_data_dir = "car_rocket/test"
-nb_train_samples = 600
-nb_validation_samples = 600
-batch_size = 600
-epochs = 3
 
-X_train = np.load('X_train.npy')
-y_train = np.load('y_train.npy')
-X_test = np.load('X_test.npy')
-y_test = np.load('y_test.npy')
+
+load_dir = "assets/car-rocket-ship/imnet_nofc/VGG19/128x128/"
+X_train = np.load(load_dir + 'X_train.npy')
+y_train = np.load(load_dir + 'y_train.npy')
+X_test = np.load(load_dir + 'X_test.npy')
+y_test = np.load(load_dir + 'y_test.npy')
 N_tr = X_train.shape[0]
 N_te = X_test.shape[0]
 X_train = X_train.reshape(N_tr,-1)
-X_test = X_train.reshape(N_te,-1)
+X_test = X_test.reshape(N_te,-1)
 in_size = X_train.shape[1]
-#Adding custom Layers 
-reg = 10000
-model = Sequential([
-	Dense(3,input_shape=(in_size,),kernel_regularizer=regularizers.l2(reg)),
-	Activation('softmax')
-	])
+#choosing bewtween different network architectures  
+reg=0.001
+if 0:
+    model = Sequential([
+        Dense(100,input_shape=(in_size,),kernel_regularizer=regularizers.l2(reg)),
+        Activation('relu'),
+        Dropout(0.3),
+        Dense(50,kernel_regularizer=regularizers.l2(reg)),
+        Activation('relu'),
+        Dropout(0.3),
+        Dense(3,kernel_regularizer=regularizers.l2(reg)),
+        Activation('softmax')
+        ])
+if 0:
+    model = Sequential([
+        Dense(3,input_shape=(in_size,),kernel_regularizer=regularizers.l2(0.001)),
+        Activation('softmax')
+        ])
 
-model.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
-model.fit(X_train,y_train,
-	validation_data=(X_test,y_test),
-	batch_size = 50,
-	epochs=50)
+if 1:
+    model = Sequential([
+        #Dropout(0,input_shape=(in_size,)),
+        Dense(10,input_shape=(in_size,),kernel_regularizer=regularizers.l2(0.5)),
+        Activation('relu'),
+        Dense(3,kernel_regularizer=regularizers.l2(reg)),
+        Activation('softmax')
+        ])
+
+# Train the model and plot training info
+model.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.001, momentum=0.9), metrics=["accuracy"])
+hist = model.fit(X_train,y_train,
+    validation_data=(X_test,y_test),
+    batch_size = 50,
+    epochs=15,
+    verbose=2
+    )
+plt.plot(hist.history['acc'], label="train")
+plt.plot(hist.history['val_acc'], label="val")
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.show()
